ec65e2032ca2a302ce24f77f89427533d37ece66
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-07-02 22:24:04 +0900

    Install account abstraction from eth infinitism

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount is IAccount {
+    // entrypoint will eventuall call this contract
+
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData) {}
+}
